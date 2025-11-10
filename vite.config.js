fe7c07c8514d2880b0c70e0fa89edc40f056ada3
Sloth Deploy <devops@sloth.local>
2025-11-10 15:30:26 +0100

    fix: force new hash in vite build to clear cache

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Force new hash on every build
+        entryFileNames: `assets/[name]-${Date.now()}-[hash].js`,
+        chunkFileNames: `assets/[name]-${Date.now()}-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
